becd0e9765634851f8245937dae303af7d2b9c43
willfox99 <william_fox@asl.org>
2014-10-14 08:16:06 +0100

    Project 1

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, Github");
+    System.out.println("Hello World");
+    System.out.println("This is very simple Java Code");
+    
   }
 }
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ sample
 ======
 This is a sample repo for my students to fork.
 Hello
+
+let b = 6
+let c = 6
+
+console.log(b+c)
